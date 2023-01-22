9a3476e126f7cf041057911bcc6d5b7f36843bf1
Gustavo <gustavo27hs@gmail.com>
2023-01-22 20:59:57 -0300

    Inicio backEnd do projeto Karas

---
+++ src/main/java/com/karas/karasback/KarasBackApplication.java
@@ -0,0 +1,13 @@
+package com.karas.karasback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KarasBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KarasBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/karas/karasback/KarasBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karas.karasback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KarasBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
